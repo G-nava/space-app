066bcc6ad80fff9fab56bb97f3793bd29599dad7
G-nava <navarro9207@gmail.com>
2022-07-17 22:31:44 -0500

    mesage

--- reg.js
+++ reg.js
@@ -1,13 +1,13 @@
 
-const detectRegex = path => new RegExp('^'+ path.replace(/\//g, '\\/').replace(/:\w+/g, '.+') + '$');
+const detectRegex = path => new RegEx('^'+ path.replace(/\//g, '\\/').replace(/:\w+/g, '.+') + '$');
 
 
-const r = '/post/2'
+const r = '/post/5'
 
 const ruta =[
     {path: '/post/:id'}
 
 ]; 
-
-
+   
+ 
 console.log(r.match(/^\/post\/.+$/));
