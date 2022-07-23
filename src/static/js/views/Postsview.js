ae9f19bc88c5302af4136d16e8248c945e66c4fa
G-nava <navarro9207@gmail.com>
2022-07-22 22:09:27 -0500

    test

--- src/static/js/views/Postsview.js
+++ src/static/js/views/Postsview.js
@@ -12,6 +12,7 @@ export default class extends AbstractView {
         con este parametro puedo hacer llamadas al server site 
         para obtener la informacion para el id que se indique
         */
+       
         return `
             <h1>Posts view</h1>
             <p>
